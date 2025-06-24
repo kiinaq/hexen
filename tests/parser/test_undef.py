a0693071e17d641a41b46feda097d37d6b1bed98
kiinaq <kiinaq42@gmail.com>
2025-06-24 14:14:37 +0200

    Phase 5.2: Migrate test_undef.py to NodeType enum (8 changes)
    - Replace string-based node type references with NodeType enum values
    - Migrate val_declaration, mut_declaration, and identifier type references
    - All tests pass: 7 individual tests + 415 full suite

--- tests/parser/test_undef.py
+++ tests/parser/test_undef.py
@@ -5,6 +5,7 @@ Tests uninitialized variable declarations with undef keyword.
 """
 
 from src.hexen.parser import HexenParser
+from src.hexen.ast_nodes import NodeType
 
 
 class TestUndefDeclarations:
@@ -23,7 +24,8 @@ class TestUndefDeclarations:
         """
         result = self.parser.parse(code)
         assert (
-            result["functions"][0]["body"]["statements"][0]["type"] == "val_declaration"
+            result["functions"][0]["body"]["statements"][0]["type"]
+            == NodeType.VAL_DECLARATION.value
         )
         assert result["functions"][0]["body"]["statements"][0]["name"] == "x"
         assert (
@@ -31,7 +33,7 @@ class TestUndefDeclarations:
         )
         assert (
             result["functions"][0]["body"]["statements"][0]["value"]["type"]
-            == "identifier"
+            == NodeType.IDENTIFIER.value
         )
         assert (
             result["functions"][0]["body"]["statements"][0]["value"]["name"] == "undef"
@@ -47,7 +49,7 @@ class TestUndefDeclarations:
         """
         result = self.parser.parse(code)
         stmt = result["functions"][0]["body"]["statements"][0]
-        assert stmt["type"] == "mut_declaration"
+        assert stmt["type"] == NodeType.MUT_DECLARATION.value
         assert stmt["name"] == "y"
         assert stmt["type_annotation"] == "string"
         assert stmt["value"]["name"] == "undef"
@@ -66,19 +68,19 @@ class TestUndefDeclarations:
         statements = result["functions"][0]["body"]["statements"]
 
         # val inferred_int = 42
-        assert statements[0]["type"] == "val_declaration"
+        assert statements[0]["type"] == NodeType.VAL_DECLARATION.value
         assert statements[0]["name"] == "inferred_int"
         assert statements[0]["type_annotation"] is None
         assert statements[0]["value"]["value"] == 42
 
         # val inferred_string = "hello"
-        assert statements[1]["type"] == "val_declaration"
+        assert statements[1]["type"] == NodeType.VAL_DECLARATION.value
         assert statements[1]["name"] == "inferred_string"
         assert statements[1]["type_annotation"] is None
         assert statements[1]["value"]["value"] == "hello"
 
         # mut mutable_int = 123
-        assert statements[2]["type"] == "mut_declaration"
+        assert statements[2]["type"] == NodeType.MUT_DECLARATION.value
         assert statements[2]["name"] == "mutable_int"
         assert statements[2]["type_annotation"] is None
         assert statements[2]["value"]["value"] == 123
@@ -144,7 +146,7 @@ class TestUndefDeclarations:
         """
         result = self.parser.parse(code)
         stmt = result["functions"][0]["body"]["statements"][0]
-        assert stmt["type"] == "mut_declaration"
+        assert stmt["type"] == NodeType.MUT_DECLARATION.value
         assert stmt["name"] == "precise"
         assert stmt["type_annotation"] == "f32"
         assert stmt["value"]["name"] == "undef"
@@ -159,7 +161,7 @@ class TestUndefDeclarations:
         """
         result = self.parser.parse(code)
         stmt = result["functions"][0]["body"]["statements"][0]
-        assert stmt["type"] == "val_declaration"
+        assert stmt["type"] == NodeType.VAL_DECLARATION.value
         assert stmt["name"] == "flag"
         assert stmt["type_annotation"] == "bool"
         assert stmt["value"]["name"] == "undef"
